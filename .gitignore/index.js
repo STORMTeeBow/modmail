44bbd64060c19ede57c5d369caf1efb5bbd6366b
STORMTeeBow <45888435+STORMTeeBow@users.noreply.github.com>
2018-12-15 03:03:26 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,5 +1,5 @@
 {
-  "token": "NDc4ODkyNzE1MDczODYzNjgx.Dr_rYA.9bowxN2VwrJzA5397TXPozzMCLs",
+  "token": (process.env.TOKEN),
   "mailGuildId": "501111769402441747",
   "mainGuildId": "497909540831100928",
   "logChannelId": "508508188350611467",
