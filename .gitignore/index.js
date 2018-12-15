56a8ca27e6b04c6d7d4a6b3b6d90daff65c7b253
STORMTeeBow <45888435+STORMTeeBow@users.noreply.github.com>
2018-12-15 02:58:02 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,9 @@
+{
+  "token": "NDc4ODkyNzE1MDczODYzNjgx.Dr_rYA.9bowxN2VwrJzA5397TXPozzMCLs",
+  "mailGuildId": "501111769402441747",
+  "mainGuildId": "497909540831100928",
+  "logChannelId": "508508188350611467",
+
+  "status": "MP for support",
+  "responseMessage": "Merci d'avoir contacté le support ! Nous vous répondrons le plus vite possible !"
+}
